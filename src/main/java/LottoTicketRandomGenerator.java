f00c888f4b153dd8ed4270c4c4410ef00ae678f9
jungin0507 <a01033046758@gmail.com>
2023-01-02 17:43:54 +0900

    feat: 로또 티켓 랜덤으로 생성 기능 구현

---
+++ src/main/java/LottoTicketRandomGenerator.java
@@ -0,0 +1,16 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoTicketRandomGenerator implements LottoTicketGenerator {
+
+    @Override
+    public LottoTicket generate() {
+        List<LottoNumber> lottoNumberCandidates = IntStream.range(LottoNumber.MINIMUM_VALUE, LottoNumber.MAXIMUM_VALUE)
+                .mapToObj(LottoNumber::new)
+                .collect(Collectors.toList());
+        Collections.shuffle(lottoNumberCandidates);
+        return new LottoTicket(lottoNumberCandidates.subList(0, LottoTicket.LOTTO_NUMBERS_SIZE));
+    }
+}
