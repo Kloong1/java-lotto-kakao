67d74fae650138588e190c0fe2efdafc0ab018c6
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-03 16:29:02 +0900

    feat: 로또 등수를 판단하는 LottoNumberMatcher 구현
    LottoPrize가 다른 도메인 객체에 의존하지 않게 하기 위해서 등수를
    판단하는 로직을 LottoNumberMatcher에 위임함

--- src/main/java/LottoNumberMatcher.java
+++ src/main/java/LottoNumberMatcher.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class LottoNumberMatcher {
+import domain.lotto.LottoPrize;
+import domain.lotto.LottoTicket;
+import domain.lotto.LottoWinningNumber;
+
+import java.util.Arrays;
+
+public class LottoNumberMatcher {
+    private final LottoWinningNumber lottoWinningNumber;
+
+    public LottoNumberMatcher(LottoWinningNumber lottoWinningNumber) {
+        this.lottoWinningNumber = lottoWinningNumber;
+    }
+
+    public LottoPrize match(LottoTicket lottoTicket) {
+        return Arrays.stream(LottoPrize.values())
+                .filter(lottoPrize -> lottoPrize.prizeJudgment.judge(lottoTicket, lottoWinningNumber))
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
+    }
 }
