9b59e5ed23dfe885fea3e6e2eac7d98de5b7d4e0
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-03 18:24:22 +0900

    feat: LottoPrizeResult의 earningRate 변수와 관련 로직 삭제

--- src/main/java/domain/lotto/LottoPrizeResult.java
+++ src/main/java/domain/lotto/LottoPrizeResult.java
@@ -1,36 +1,31 @@
 package domain.lotto;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 
 public class LottoPrizeResult {
 
     private final Map<LottoPrize, Integer> prizeCounts;
-    private double earningRate;
 
     public LottoPrizeResult(Map<LottoPrize, Integer> prizeCounts) {
         validatePrizeCounts(prizeCounts);
         this.prizeCounts = prizeCounts;
     }
 
-    public LottoPrizeResult(Map<LottoPrize, Integer> prizeCounts, double earningRate) {
-        this(prizeCounts);
-        this.earningRate = earningRate;
-    }
-
     public Map<LottoPrize, Integer> getPrizeCounts() {
         return prizeCounts;
-    }
-
-    public double getEarningRate() {
-        return earningRate;
     }
 
     public LottoEarningRate calculateEarningRate(int lottoPurchaseCount) {
         validateLottoPurchaseCount(lottoPurchaseCount);
         double cost = (double) lottoPurchaseCount * LottoShop.LOTTO_PRICE;
         return new LottoEarningRate(getPrizeMoneySum() / cost);
+    }
+
+    private void validatePrizeCounts(Map<LottoPrize, Integer> prizeCounts) {
+        if (prizeCounts.size() != LottoPrize.values().length) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validateLottoPurchaseCount(int lottoPurchaseCount) {
@@ -43,12 +38,6 @@ public class LottoPrizeResult {
         return prizeCounts.entrySet().stream()
                 .mapToInt(prizeCount -> prizeCount.getKey().getPrizeMoney() * prizeCount.getValue())
                 .sum();
-    }
-
-    private void validatePrizeCounts(Map<LottoPrize, Integer> prizeCounts) {
-        if (prizeCounts.size() != LottoPrize.values().length) {
-            throw new IllegalArgumentException();
-        }
     }
 
     @Override
