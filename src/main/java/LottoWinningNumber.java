8919254f92ec158f43d7f6d891f2cbbff076436a
jungin0507 <a01033046758@gmail.com>
2023-01-03 11:12:53 +0900

    feat: LottoWinningNumber 구현

---
+++ src/main/java/LottoWinningNumber.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class LottoWinningNumber {
+
+    private final List<LottoNumber> lottoNumbers;
+    private final LottoNumber bonusNumber;
+
+    public LottoWinningNumber(List<LottoNumber> lottoNumbers, LottoNumber bonusNumber) {
+        validateSize(lottoNumbers);
+        validateDuplicates(lottoNumbers, bonusNumber);
+        this.lottoNumbers = lottoNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<LottoNumber> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public LottoNumber getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validateSize(List<LottoNumber> lottoNumbers) {
+        if (lottoNumbers.size() != LottoTicket.LOTTO_NUMBERS_SIZE) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    public void validateDuplicates(List<LottoNumber> lottoNumbers, LottoNumber bonusNumber) {
+        if (ListUtils.hasDuplicates(lottoNumbers) || lottoNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("중복된 로또 번호가 있습니다.");
+        }
+    }
+}
