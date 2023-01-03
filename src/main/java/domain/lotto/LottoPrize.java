e4ea95a8800283a5ec2989b43ae5f1dc403653bf
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-03 18:23:04 +0900

    feat: LottoPrize의 당첨 여부 판단 로직 제거

--- src/main/java/domain/lotto/LottoPrize.java
+++ src/main/java/domain/lotto/LottoPrize.java
@@ -3,8 +3,6 @@ package domain.lotto;
 import domain.judgment.LottoPrizeJudgment;
 import domain.judgment.impl.*;
 
-import java.util.Arrays;
-
 public enum LottoPrize {
 
     FIRST_PRIZE(2_000_000_000, new FirstLottoPrizeJudgment()),
@@ -25,12 +23,4 @@ public enum LottoPrize {
     public int getPrizeMoney() {
         return prizeMoney;
     }
-
-    public static LottoPrize findPrize(LottoTicket lottoTicket, LottoWinningNumber lottoWinningNumber) {
-        return Arrays.stream(values())
-                .filter(e -> e.prizeJudgment.judge(lottoTicket, lottoWinningNumber))
-                .findFirst()
-                .orElseThrow(IllegalStateException::new);
-    }
-
 }
