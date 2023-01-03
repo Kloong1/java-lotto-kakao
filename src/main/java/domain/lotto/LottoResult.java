ea53a0edc22405ba0e6eef9d4643fc177a6ac32f
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-03 17:09:50 +0900

    feat: LottoResult에 검증 로직 구현

--- src/main/java/domain/lotto/LottoResult.java
+++ src/main/java/domain/lotto/LottoResult.java
@@ -1,14 +1,20 @@
 package domain.lotto;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class LottoResult {
 
     private final Map<LottoPrize, Integer> prizeCounts;
-    private final double earningRate;
+    private double earningRate;
+
+    public LottoResult(Map<LottoPrize, Integer> prizeCounts) {
+        validatePrizeCounts(prizeCounts);
+        this.prizeCounts = prizeCounts;
+    }
 
     public LottoResult(Map<LottoPrize, Integer> prizeCounts, double earningRate) {
-        this.prizeCounts = prizeCounts;
+        this(prizeCounts);
         this.earningRate = earningRate;
     }
 
@@ -18,5 +24,13 @@ public class LottoResult {
 
     public double getEarningRate() {
         return earningRate;
+    }
+
+    private void validatePrizeCounts(Map<LottoPrize, Integer> prizeCounts) {
+        boolean hasMissingPrize = Arrays.stream(LottoPrize.values())
+                .anyMatch(lottoPrize -> !prizeCounts.containsKey(lottoPrize));
+        if (hasMissingPrize) {
+            throw new IllegalArgumentException();
+        }
     }
 }
---
+++ src/test/java/LottoResultTest.java
@@ -0,0 +1,38 @@
+import domain.lotto.LottoPrize;
+import domain.lotto.LottoResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoResultTest {
+
+    HashMap<LottoPrize, Integer> prizeCounts;
+
+    @BeforeEach
+    void setUp() {
+        prizeCounts = new HashMap<>();
+        prizeCounts.put(LottoPrize.FIRST_PRIZE, 0);
+        prizeCounts.put(LottoPrize.SECOND_PRIZE, 0);
+        prizeCounts.put(LottoPrize.THIRD_PRIZE, 0);
+        prizeCounts.put(LottoPrize.FOURTH_PRIZE, 0);
+        prizeCounts.put(LottoPrize.FIFTH_PRIZE, 0);
+        prizeCounts.put(LottoPrize.NONE_PRIZE, 0);
+    }
+
+    @DisplayName("Map<LottoPrize, Integer>를 주입받아 LottoResult를 생성한다")
+    @Test
+    void create() {
+        assertThatNoException().isThrownBy(() -> new LottoResult(prizeCounts));
+    }
+
+    @DisplayName("잘못된 등수 카운트가 들어오면 예외가 발생한다")
+    @Test
+    void create_throw() {
+        prizeCounts.remove(LottoPrize.FIRST_PRIZE);
+        assertThatIllegalArgumentException().isThrownBy(() -> new LottoResult(prizeCounts));
+    }
+}
