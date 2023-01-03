53c804171a39f0586bd3cdd8913e8c9a19eb697b
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-03 18:07:47 +0900

    feat: 로또 당첨 결과인 LottoPrizeResult를 반환한다

--- src/main/java/LottoPrizeCounter.java
+++ src/main/java/LottoPrizeCounter.java
@@ -1,6 +1,29 @@
+import domain.lotto.LottoPrize;
+import domain.lotto.LottoPrizeResult;
+import domain.lotto.LottoTicket;
 import domain.lotto.LottoWinningNumber;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LottoPrizeCounter {
+    private final LottoNumberMatcher lottoNumberMatcher;
+
     public LottoPrizeCounter(LottoWinningNumber lottoWinningNumber) {
+        this.lottoNumberMatcher = new LottoNumberMatcher(lottoWinningNumber);
+    }
+
+    public LottoPrizeResult countPrize(List<LottoTicket> lottoTickets) {
+        Map<LottoPrize, Integer> prizeCounts = new HashMap<>();
+        Arrays.stream(LottoPrize.values())
+                .forEach(prize -> prizeCounts.put(prize, 0));
+
+        lottoTickets.stream()
+                .map(lottoNumberMatcher::match)
+                .forEach(lottoPrize -> prizeCounts.computeIfPresent(lottoPrize, (key, value) -> value + 1));
+
+        return new LottoPrizeResult(prizeCounts);
     }
 }
--- src/test/java/LottoPrizeCounterTest.java
+++ src/test/java/LottoPrizeCounterTest.java
@@ -1,11 +1,11 @@
-import domain.lotto.LottoNumber;
-import domain.lotto.LottoWinningNumber;
+import domain.lotto.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class LottoPrizeCounterTest {
@@ -32,5 +32,36 @@ public class LottoPrizeCounterTest {
     @Test
     void create() {
         Assertions.assertThatNoException().isThrownBy(() -> new LottoPrizeCounter(lottoWinningNumber));
+    }
+
+    @DisplayName("로또 당첨 결과를 반환한다.")
+    @Test
+    void countPrize() {
+        List<LottoTicket> lottoTickets = List.of(
+                new LottoTicket(Arrays.asList(
+                        new LottoNumber(1),
+                        new LottoNumber(2),
+                        new LottoNumber(3),
+                        new LottoNumber(4),
+                        new LottoNumber(5),
+                        new LottoNumber(6)
+                ))
+        );
+
+        HashMap<LottoPrize, Integer> expectedMap = new HashMap<>();
+        expectedMap.put(LottoPrize.FIRST_PRIZE, 1);
+        expectedMap.put(LottoPrize.SECOND_PRIZE, 0);
+        expectedMap.put(LottoPrize.THIRD_PRIZE, 0);
+        expectedMap.put(LottoPrize.FOURTH_PRIZE, 0);
+        expectedMap.put(LottoPrize.FIFTH_PRIZE, 0);
+        expectedMap.put(LottoPrize.NONE_PRIZE, 0);
+
+        LottoPrizeResult expected = new LottoPrizeResult(expectedMap);
+
+        LottoPrizeCounter lottoPrizeCounter = new LottoPrizeCounter(lottoWinningNumber);
+
+        LottoPrizeResult result = lottoPrizeCounter.countPrize(lottoTickets);
+
+        Assertions.assertThat(result).isEqualTo(expected);
     }
 }
