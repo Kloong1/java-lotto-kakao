db80993c30e5b88ad2ffddd506bf55d33a18c34f
jungin0507 <a01033046758@gmail.com>
2023-01-03 11:12:28 +0900

    feat: ListUtil 구현

---
+++ src/main/java/ListUtils.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class ListUtils {
+
+    public static <T> boolean hasDuplicates(List<T> list) {
+        long distinctCount = list.stream()
+                .distinct()
+                .count();
+        return distinctCount < list.size();
+    }
+
+    public static <T> long countCommonElements(List<T> list1, List<T> list2) {
+        return list2.stream()
+                .filter(list1::contains)
+                .count();
+    }
+}
