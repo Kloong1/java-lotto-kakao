722718650a3aaf55f21b0a71fe5c2a14daed17b4
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-05 00:50:24 +0900

    feat: equals(), hashCode() 구현

--- src/main/java/domain/lotto/LottoNumbers.java
+++ src/main/java/domain/lotto/LottoNumbers.java
@@ -32,4 +32,17 @@ public class LottoNumbers {
             throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumbers that = (LottoNumbers) o;
+        return Objects.equals(numbers, that.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
 }
