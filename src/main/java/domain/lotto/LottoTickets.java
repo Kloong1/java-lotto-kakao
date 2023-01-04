53a88cf486e0aef039e687f6f3c25f4c3003df7f
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-05 01:49:42 +0900

    feat: 비어있는 LottoNumbers List로 LottoTickets를 생성하면 예외 발생

--- src/main/java/domain/lotto/LottoTickets.java
+++ src/main/java/domain/lotto/LottoTickets.java
@@ -7,6 +7,13 @@ public class LottoTickets {
     private final List<LottoNumbers> tickets;
 
     public LottoTickets(List<LottoNumbers> tickets) {
+        validateTickets(tickets);
         this.tickets = new ArrayList<>(tickets);
+    }
+
+    private void validateTickets(List<LottoNumbers> tickets) {
+        if (tickets == null || tickets.isEmpty()) {
+            throw new IllegalArgumentException("로또 복권은 한 장 이상이어야 합니다.");
+        }
     }
 }
--- src/test/java/domain/lotto/LottoTicketsTest.java
+++ src/test/java/domain/lotto/LottoTicketsTest.java
@@ -1,26 +1,40 @@
 package domain.lotto;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class LottoTicketsTest {
+    LottoNumbersGenerator lottoNumbersGenerator;
+
+    @BeforeEach
+    void setUp() {
+        lottoNumbersGenerator = new LottoNumbersRandomGenerator(LottoNumber.MINIMUM_NUMBER, LottoNumber.MAXIMUM_NUMBER);
+    }
 
     @DisplayName("LottoNumbers의 List를 넘겨받아서 LottoTickets를 생성한다.")
     @Test
     void create() {
-        LottoNumbersRandomGenerator lottoNumbersRandomGenerator =
-                new LottoNumbersRandomGenerator(LottoNumber.MINIMUM_NUMBER, LottoNumber.MAXIMUM_NUMBER);
-
         List<LottoNumbers> lottoNumbers = IntStream.range(0, 10)
-                .mapToObj(i -> lottoNumbersRandomGenerator.generate())
+                .mapToObj(i -> lottoNumbersGenerator.generate())
                 .collect(Collectors.toList());
 
-        Assertions.assertThatNoException()
+        assertThatNoException()
                 .isThrownBy(() -> new LottoTickets(lottoNumbers));
+    }
+
+    @DisplayName("비어있는 LottoNumbers의 List가 들어오면 예외가 발생한다.")
+    @Test
+    void create_throw() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new LottoTickets(Collections.emptyList()));
     }
 }
