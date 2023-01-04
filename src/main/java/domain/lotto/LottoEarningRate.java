77f92fae17f0d82a6b0e0b0a10df1c663de8e537
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-05 02:19:43 +0900

    feat: LottoPrizeResult와 Cost를 넘겨받아 수익률 계산하는 로직 구현

--- src/main/java/domain/lotto/LottoEarningRate.java
+++ src/main/java/domain/lotto/LottoEarningRate.java
@@ -1,21 +1,43 @@
 package domain.lotto;
 
+import java.util.Objects;
+
 public class LottoEarningRate {
     public static final double MINIMUM_EARNING_RATE = 0.0;
     private final double earningRate;
 
     public LottoEarningRate(double earningRate) {
-        validatePositive(earningRate);
+        validateEarningRate(earningRate);
         this.earningRate = earningRate;
+    }
+
+    public LottoEarningRate(LottoPrizeResult lottoPrizeResult, Cost cost) {
+        int prizeMoneySum = lottoPrizeResult.calculatePrizeMoneySum();
+        double tempEarningRate = (double) prizeMoneySum / cost.getCost();
+        validateEarningRate(tempEarningRate);
+        this.earningRate = tempEarningRate;
     }
 
     public double getEarningRate() {
         return earningRate;
     }
 
-    private void validatePositive(double earningRate) {
+    private void validateEarningRate(double earningRate) {
         if (earningRate < MINIMUM_EARNING_RATE) {
             throw new IllegalArgumentException("수익률은 0 이상이어야 합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoEarningRate that = (LottoEarningRate) o;
+        return Double.compare(that.earningRate, earningRate) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(earningRate);
     }
 }
--- src/test/java/domain/lotto/LottoEarningRateTest.java
+++ src/test/java/domain/lotto/LottoEarningRateTest.java
@@ -1,9 +1,15 @@
 package domain.lotto;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class LottoEarningRateTest {
 
@@ -11,13 +17,30 @@ public class LottoEarningRateTest {
     @ParameterizedTest
     @ValueSource(doubles = {0.1, 0.123, 0.9, 123412.0, 0.0})
     void create(double earningRate) {
-        Assertions.assertThatNoException().isThrownBy(() -> new LottoEarningRate(earningRate));
+        assertThatNoException().isThrownBy(() -> new LottoEarningRate(earningRate));
     }
 
     @DisplayName("음수 수익률이 들어오면 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(doubles = {-0.1, -3.8})
     void create_throw(double earningRate) {
-        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new LottoEarningRate(earningRate));
+        assertThatIllegalArgumentException().isThrownBy(() -> new LottoEarningRate(earningRate));
+    }
+
+    @DisplayName("LottoPrizeResult와 Cost를 넘겨 받아 수익률을 계산한다")
+    @Test
+    void create_fromPrizeAndCost() {
+        Map<LottoPrize, Integer> prizeCounts = new EnumMap<>(LottoPrize.class);
+        Arrays.stream(LottoPrize.values())
+                .forEach(lottoPrize -> prizeCounts.put(lottoPrize, 0));
+        prizeCounts.put(LottoPrize.FIRST_PRIZE, 1);
+
+        LottoPrizeResult lottoPrizeResult = new LottoPrizeResult(prizeCounts);
+        Cost cost = new Cost(10_000);
+
+        LottoEarningRate lottoEarningRate = new LottoEarningRate(lottoPrizeResult, cost);
+
+        assertThat(lottoEarningRate.getEarningRate())
+                .isEqualTo((double) LottoPrize.FIRST_PRIZE.getPrizeMoney() / cost.getCost());
     }
 }
