6ab8e405834f612e498a19aba53190fac62f0988
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-05 01:18:39 +0900

    test: 당첨 번호와 보너스 번호 중복 체크하는 테스트 코드 추가

--- src/test/java/domain/lotto/LottoWinningNumberTest.java
+++ src/test/java/domain/lotto/LottoWinningNumberTest.java
@@ -1,17 +1,22 @@
 package domain.lotto;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class LottoWinningNumberTest {
 
-    @DisplayName("당첨 번호 6개와 보너스 번호 1개를 주입받아서 생성한다")
-    @Test
-    void create() {
-        LottoNumbers lottoNumbers = new LottoNumbers(Set.of(
+    LottoNumbers lottoNumbers;
+
+    @BeforeEach
+    void setUp() {
+        lottoNumbers = new LottoNumbers(Set.of(
                 new LottoNumber(1),
                 new LottoNumber(2),
                 new LottoNumber(3),
@@ -20,9 +25,24 @@ public class LottoWinningNumberTest {
                 new LottoNumber(6)
 
         ));
+    }
+
+    @DisplayName("당첨 번호 6개와 보너스 번호 1개를 주입받아서 생성한다")
+    @Test
+    void create() {
         LottoNumber bonusNumber = new LottoNumber(10);
 
-        Assertions.assertThatNoException()
+        assertThatNoException()
+                .isThrownBy(() -> new LottoWinningNumber(lottoNumbers, bonusNumber));
+    }
+
+    @DisplayName("당첨 번호 중 보너스 번호와 중복된 번호가 있으면 예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void create_throw(int number) {
+        LottoNumber bonusNumber = new LottoNumber(number);
+
+        assertThatIllegalArgumentException()
                 .isThrownBy(() -> new LottoWinningNumber(lottoNumbers, bonusNumber));
     }
 }
