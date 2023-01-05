7c8071eb211f51997cf2d3bf56f1beb674c69f93
Kloong1 <dkdlsmsm98@gmail.com>
2023-01-05 15:25:52 +0900

    feat: 로또 번호 수동 입력 로직 구현

--- src/main/java/view/LottoInputView.java
+++ src/main/java/view/LottoInputView.java
@@ -2,10 +2,9 @@ package view;
 
 import domain.lotto.*;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoInputView {
 
@@ -24,28 +23,34 @@ public class LottoInputView {
         return new LottoTicketPurchaseAmount(purchaseAmount, purchaseCost);
     }
 
+    public List<LottoNumbers> readManualLottoNumbers(LottoTicketPurchaseAmount lottoTicketPurchaseAmount) {
+        System.out.println(System.lineSeparator() + "수동으로 구매할 로또 번호를 입력해 주세요.");
+        return IntStream.range(0, lottoTicketPurchaseAmount.getPurchaseAmount())
+                .mapToObj(idx -> readLottoNumbers())
+                .collect(Collectors.toList());
+    }
+
     public LottoWinningNumbers readLottoWinningNumbers() {
-        LottoNumbers winningNumbers = readWinningNumbers();
+        System.out.println(System.lineSeparator() + "지난 주 당첨 번호를 입력해 주세요.");
+        LottoNumbers winningNumbers = readLottoNumbers();
+        System.out.println("보너스 볼을 입력해 주세요.");
         LottoNumber bonusNumber = readBonusNumber();
         return new LottoWinningNumbers(winningNumbers, bonusNumber);
     }
 
-    private LottoNumbers readWinningNumbers() {
-        System.out.println(System.lineSeparator() + "지난 주 당첨 번호를 입력해 주세요.");
+    private LottoNumbers readLottoNumbers() {
         String input = sc.nextLine();
         String[] inputStrings = input.split(",");
-        Set<LottoNumber> winningNumbers = Arrays.stream(inputStrings)
+        Set<LottoNumber> lottoNumbers = Arrays.stream(inputStrings)
                 .map(String::trim)
                 .mapToInt(Integer::parseInt)
                 .mapToObj(LottoNumber::new)
                 .collect(Collectors.toSet());
-        return new LottoNumbers(winningNumbers);
+        return new LottoNumbers(lottoNumbers);
     }
 
     private LottoNumber readBonusNumber() {
-        System.out.println("보너스 볼을 입력해 주세요.");
         int bonusBallNumber = Integer.parseInt(sc.nextLine());
-
         return new LottoNumber(bonusBallNumber);
     }
 }
